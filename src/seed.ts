1810f39b4bfacb84bbbbf7ba481ea5e52024a1ee
unknown <pimprikarsarthak@gmail.com>
2026-04-28 17:52:26 +0530

    Update: Backend seed and package dependencies

--- src/seed.ts
+++ src/seed.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import dotenv from 'dotenv';
 import User from './models/User';
 import Plan from './models/Plan';
